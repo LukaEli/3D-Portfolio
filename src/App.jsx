b5b660670283cbc4da3b850aae5d86121afeadbc
CloneTrooper66 <luka.eli88@gmail.com>
2024-09-17 18:21:00 +0100

    created all sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Works from "./components/Works";
+import Contact from "./components/Contact";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("img/bg.jpeg");
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
   );
 }
 
